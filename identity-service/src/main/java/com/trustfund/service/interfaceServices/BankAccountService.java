739bcf5fa69821624481aa21ada38ed52be2dec5
thanghung18062003 <hungpltse172380@fpt.edu.vn>
2026-01-18 01:21:51 +0700

    API PATCH /api/bank-accounts/{id}/status

--- identity-service/src/main/java/com/trustfund/service/interfaceServices/BankAccountService.java
+++ identity-service/src/main/java/com/trustfund/service/interfaceServices/BankAccountService.java
@@ -1,6 +1,7 @@
 package com.trustfund.service.interfaceServices;
 
 import com.trustfund.model.request.CreateBankAccountRequest;
+import com.trustfund.model.request.UpdateBankAccountStatusRequest;
 import com.trustfund.model.response.BankAccountResponse;
 
 import java.util.List;
@@ -9,4 +10,6 @@ public interface BankAccountService {
     BankAccountResponse create(CreateBankAccountRequest request, String currentEmail);
 
     List<BankAccountResponse> getMyBankAccounts(Long userId);
+
+    BankAccountResponse updateStatus(Long bankAccountId, UpdateBankAccountStatusRequest request, Long currentUserId, String currentRole);
 }
