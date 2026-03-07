89cb81013dff51db941fc372bfc90f1c881939e2
Authur0912 <trinhtbdse171941@fpt.edu.vn>
2026-03-07 21:29:07 +0700

    fix: remove legacy updateRejectedExpenditure code

--- campaign-service/src/main/java/com/trustfund/repository/ExpenditureItemRepository.java
+++ campaign-service/src/main/java/com/trustfund/repository/ExpenditureItemRepository.java
@@ -2,7 +2,6 @@ package com.trustfund.repository;
 
 import com.trustfund.model.ExpenditureItem;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,9 +9,6 @@ import java.util.List;
 @Repository
 public interface ExpenditureItemRepository extends JpaRepository<ExpenditureItem, Long> {
     List<ExpenditureItem> findByExpenditureId(Long expenditureId);
-
-    @Modifying
-    void deleteByExpenditureId(Long expenditureId);
 
     @org.springframework.data.jpa.repository.Query("SELECT ei FROM ExpenditureItem ei JOIN ei.expenditure e WHERE e.campaignId = :campaignId")
     List<ExpenditureItem> findByExpenditureCampaignId(
